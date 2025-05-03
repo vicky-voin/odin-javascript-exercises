0fa56e470b0cc0c6b9e8e79aefe528880ec80a4c
vicky-voin <alteneria127@gmail.com>
2025-05-03 12:59:43 -0400

    Finished exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(input, repeatCount) {
+    if(repeatCount < 0)
+    {
+        return "ERROR";
+    }
+    
+    let finalString = "";
+    while(repeatCount > 0)
+    {
+        finalString += input;
+        repeatCount--;
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
