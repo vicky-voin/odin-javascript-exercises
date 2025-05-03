71d68c8ed723e790b35501e774399573f9e23829
vicky-voin <alteneria127@gmail.com>
2025-05-03 13:17:40 -0400

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbersArray, ...numsToRemove) {
+    let modifiedArray = Array();
+    for(let i = 0; i < numbersArray.length; i++)
+    {
+        let shouldRemove = false;
+
+        for(const numToRemove of numsToRemove)
+        {
+            if(numToRemove === numbersArray[i])
+                shouldRemove = true;
+        }
+
+        if(!shouldRemove)
+        {
+            modifiedArray.push(numbersArray[i]);
+        }
+    }
+
+    return modifiedArray;
 };
 
 // Do not edit below this line
