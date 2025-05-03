ae1b633886a45a924698cff51a6462e4b23944da
vicky-voin <alteneria127@gmail.com>
2025-05-03 13:05:25 -0400

    Finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversedString = "";
+    for(let i = input.length-1; i >=0; i--)
+    {
+        reversedString += input.at(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
