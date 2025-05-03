36065c182e7b3c0f3ad9a2d722b14a7c318095e2
vicky-voin <alteneria127@gmail.com>
2025-05-03 13:27:57 -0400

    Finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    if(begin < 0 || end < 0)
+        return "ERROR";
 
+    if(!Number.isInteger(begin) || !Number.isInteger(end))
+        return "ERROR";
+
+    let finalSum = 0;
+
+    for(let i = Math.min(begin, end); i <= Math.max(begin, end); i++)
+    {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
