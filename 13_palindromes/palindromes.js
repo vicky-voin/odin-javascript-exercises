38243f578681131894bbd3536b6fcaa1a10776ce
vicky-voin <alteneria127@gmail.com>
2025-05-23 13:13:20 -0400

    Finished palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let cleanedString = input
+    .toLowerCase()
+    .split('')
+    .filter(char => alphanumerical.includes(char))
+    .join('');
+
+    let reversedString = cleanedString.split('').reverse().join('');
+
+    return reversedString === cleanedString;
 };
 
 // Do not edit below this line
