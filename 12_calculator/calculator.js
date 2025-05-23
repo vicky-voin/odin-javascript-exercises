b44dea0d446d73e1427847088d75a1eeb9b5b5e5
vicky-voin <alteneria127@gmail.com>
2025-05-23 12:49:10 -0400

    Finished calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((sum, item) => sum += item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((mult, curr) => mult *= curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1;
+	for(let i = 2; i <= a; i++)
+  {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
