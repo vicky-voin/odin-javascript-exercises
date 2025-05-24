10b0dd749dbfda202bb2c41fafbd5dfb266eef0e
vicky-voin <alteneria127@gmail.com>
2025-05-24 10:29:04 -0400

    Completed fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let preceeding = [1,1];
 
+    if(n < 3)
+    {
+        if(n == 0)
+            return 0;
+        else if (n<0)
+            return "OOPS";
+        else
+            return preceeding[n-1];
+    }
+
+    let fibonacci = 0;
+
+    for(let i = 3; i <= n; i++)
+    {
+        fibonacci = preceeding[0] + preceeding[1];
+        preceeding.shift();
+        preceeding.push(fibonacci);
+    }
+
+    return fibonacci;
 };
 
 // Do not edit below this line
